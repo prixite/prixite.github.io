655d750fc00fcdf88d7cd88c091b2d369ff51251
uzairmirza <53081780+uzairmirza90@users.noreply.github.com>
2022-05-17 13:44:50 +0500

    header bug fixed

--- v2/prixite/components/header/Header.js
+++ v2/prixite/components/header/Header.js
@@ -7,113 +7,120 @@ import { pages } from '../../constants/constants';
 import { useRouter } from 'next/router';
 
 const Header = ({ children, bgImg }) => {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
 
-    const router = useRouter()
+  const router = useRouter()
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
 
-    const handleCloseNavMenu = (page) => {
-        setAnchorElNav(null)
-        router.push(page.path)
-    };
+  const handleCloseNavMenu = (page) => {
+    if (page.text === 'Home') {
+      router.push('/')
+    } else if (page.text === 'Careers') {
+      router.push('/careers')
+    } else if (page.text === 'Contact Us') {
+      router.push('/contact-us')
+    }
 
-    return (
-        <Box
-            sx={{
-                background: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${bgImg?.src})`,
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover'
-            }}
-        >
-            <Box className='header'></Box>
-            <AppBar position='static' className='app-bar'>
-                <Container maxWidth='xl'>
-                    <Toolbar disableGutters>
-                        <Box sx={{ mr: 2, display: { xs: "none", md: "flex" }, cursor: 'pointer' }} >
-                            <Image src={logoImg} alt='Prixite' onClick={() => router.push('/')} />
-                        </Box>
-                        <Box sx={{ flexGrow: 1 }}></Box>
-                        <Box sx={{ flexGrow: 50, display: { xs: "flex", md: "none" }, cursor: 'pointer' }} >
-                            <Image src={logoImg} alt="Prixite" onClick={() => router.push('/')}/>
-                        </Box>
-                        <Box sx={{ flexGrow: 0, display: { xs: "flex", md: "none" } }} >
-                            <IconButton
-                                size='large'
-                                aria-label="account of current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleOpenNavMenu}
-                                color="inherit"
-                            >
-                                <MenuIcon />
-                            </IconButton>
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorElNav}
-                                anchorOrigin={{
-                                    vertical: "bottom",
-                                    horizontal: "left",
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: "top",
-                                    horizontal: "left",
-                                }}
-                                open={Boolean(anchorElNav)}
-                                onClose={handleCloseNavMenu}
-                                sx={{ display: { xs: "block", md: "none" } }}
-                            >
-                                {pages.map((page, index) => (
-                                    <MenuItem key={index} onClick={() => handleCloseNavMenu(page)}>
-                                        <Typography textAlign="center">{page.text}</Typography>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Box>
+    setAnchorElNav(null)
+  };
 
-                        <Box
-                            className='header-menu'
-                            sx={{ display: { xs: 'none', md: 'flex' } }}
-                        >
-                            {pages.map((page, index) => {
-                                if (page.text == 'Contact Us') {
-                                    return (
-                                        <Link href={page?.path} sx={{ textDecoration: 'none' }} key={index}>
-                                            <Button
-                                                className='contact-us-button'
-                                                sx={{ mx: 2 }}
-                                            >
-                                                {page.text}
-                                            </Button>
-                                        </Link>
-                                    )
-                                }
-                                return (
-                                    <Link href={page?.path} sx={{ textDecoration: 'none' }} key={index}>
-                                        <Typography
-                                            className='header-menu-items'
-                                            sx={{
-                                                mx: 2,
-                                                color: router.asPath == page.path ? "var(--primary-green)" : "white",
-                                            }}
-                                            textAlign='center'
-                                        >
-                                            {page.text}
-                                        </Typography>
-                                    </Link>
-                                )
-                            })}
-                        </Box>
-                    </Toolbar>
-                </Container>
-            </AppBar>
-            {children}
-        </Box>
-    )
+  return (
+    <Box
+      sx={{
+        background: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${bgImg?.src})`,
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+      }}
+    >
+      <Box className='header'></Box>
+      <AppBar position='static' className='app-bar'>
+        <Container maxWidth='xl'>
+          <Toolbar disableGutters>
+            <Box sx={{ mr: 2, display: { xs: "none", md: "flex" }, cursor: 'pointer' }} >
+              <Image src={logoImg} alt='Prixite' onClick={() => router.push('/')} />
+            </Box>
+            <Box sx={{ flexGrow: 1 }}></Box>
+            <Box sx={{ flexGrow: 50, display: { xs: "flex", md: "none" }, cursor: 'pointer' }} >
+              <Image src={logoImg} alt="Prixite" onClick={() => router.push('/')} />
+            </Box>
+            <Box sx={{ flexGrow: 0, display: { xs: "flex", md: "none" } }} >
+              <IconButton
+                size='large'
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{ display: { xs: "block", md: "none" } }}
+              >
+                {pages.map((page, index) => (
+                  <MenuItem key={index} onClick={() => handleCloseNavMenu(page)}>
+                    <Typography textAlign="center">{page.text}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+
+            <Box
+              className='header-menu'
+              sx={{ display: { xs: 'none', md: 'flex' } }}
+            >
+              {pages.map((page, index) => {
+                if (page.text == 'Contact Us') {
+                  return (
+                    <Link href={page?.path} sx={{ textDecoration: 'none' }} key={index}>
+                      <Button
+                        className='contact-us-button'
+                        sx={{ mx: 2 }}
+                      >
+                        {page.text}
+                      </Button>
+                    </Link>
+                  )
+                }
+                return (
+                  <Link href={page?.path} sx={{ textDecoration: 'none' }} key={index}>
+                    <Typography
+                      className='header-menu-items'
+                      sx={{
+                        mx: 2,
+                        color: router.asPath == page.path ? "var(--primary-green)" : "white",
+                      }}
+                      textAlign='center'
+                    >
+                      {page.text}
+                    </Typography>
+                  </Link>
+                )
+              })}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      {children}
+    </Box>
+  )
 }
 
 export default Header
