6302461fea49ed62018f731551c8c85128ef88d8
uzairmirza <53081780+uzairmirza90@users.noreply.github.com>
2022-05-17 16:14:45 +0500

    crosorigin fixed

--- v2/prixite/pages/_document.js
+++ v2/prixite/pages/_document.js
@@ -5,10 +5,10 @@ export default class CustomDocument extends Document {
         return <Html>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
                 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
             </Head>
             <body>
