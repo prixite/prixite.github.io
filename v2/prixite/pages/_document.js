fadb319720b0c684b15feb12a51c4974d5927e1f
Umair Khan <umair.waheed@gmail.com>
2022-06-02 11:07:46 +0500

    Favicon (#11)
    * Add favicon.
    
    * Fix icon
    
    Co-authored-by: Umair Jameel <umair.jameel@prixite.com>
    Co-authored-by: Umair Khan <umair.khan@toptal.com>

--- v2/prixite/pages/_document.js
+++ v2/prixite/pages/_document.js
@@ -4,6 +4,7 @@ export default class CustomDocument extends Document {
     render() {
         return <Html>
             <Head>
+                <link rel="shortcut icon" href="./favicon.ico" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
