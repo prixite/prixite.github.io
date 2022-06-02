48e81f23b457d960736b8ea1517b0403b8766c48
Umair Khan <umair.waheed@gmail.com>
2022-06-02 16:35:07 +0500

    Use assetPrefix

--- v2/prixite/next.config.js
+++ v2/prixite/next.config.js
@@ -1,9 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: "akamai",
     path: "",
-  }
+  },
+  assetPrefix: isProd ? './' : '',
 }
 
 module.exports = nextConfig
