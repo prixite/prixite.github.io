a96201772e8ddf7b67dbedaa6b94ff362807b0a1
uzairmirza <53081780+uzairmirza90@users.noreply.github.com>
2022-06-09 17:20:43 +0500

    remove legacy code

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    loader: "akamai",
+    path: "/",
+  },
+  basePath: "/prixite",
+  assetPrefix: "/prixite",
+}
+
+module.exports = nextConfig
