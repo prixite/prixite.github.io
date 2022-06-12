7e49d6da6df5d076a4312805890edda231215f24
uzairmirza <53081780+uzairmirza90@users.noreply.github.com>
2022-06-12 13:36:34 +0500

    Change base path to "/" (#19)
    * Add deployment configuration
    
    * Create node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Delete node.js.yml
    
    * Create node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Delete node.js.yml
    
    * Create node.js.yml
    
    * Create deploy.yml
    
    * Delete deploy.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Delete .github/workflows directory
    
    * Create node.js.yml
    
    * Update node.js.yml
    
    * Delete node.js.yml
    
    * Create node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * yml changes
    
    * yml change
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * new deploy
    
    * new deploy
    
    * Add docs folder.
    
    * Remove docs folder.
    
    * change content
    
    * change content
    
    * add-scss styles
    
    * asd
    
    * fixed-styling for deployment
    
    * asd
    
    * asdasd
    
    * fixed footer scss
    
    * fixed style of careers page
    
    * fixed style
    
    * added favicon
    
    * fixed favicon
    
    * Delete node.js.yml
    
    * change github action configs
    
    * Update node.js.yml
    
    * Create githubactions.js.yml
    
    * Update node.js.yml
    
    * update nextconfig
    
    * Delete githubactions.js.yml
    
    * Update node.js.yml
    
    * added yarn file
    
    * change base path
    
    * Delete .github/workflows directory
    
    * Delete careers directory
    
    * Delete comments/feed directory
    
    * remove legacy code
    
    * Delete contact-us directory
    
    * Delete devops directory
    
    * Delete feed directory
    
    * Delete mobile-application-development directory
    
    * Delete open-source directory
    
    * Delete progressive-web-application directory
    
    * Delete pycurly directory
    
    * Delete python-programming directory
    
    * Delete react-development directory
    
    * Delete react-django-template directory
    
    * Delete react-flask-demo directory
    
    * Delete react-materials-ul directory
    
    * Delete react-template directory
    
    * Delete scraping directory
    
    * Delete twiter-live-feed directory
    
    * Delete v2 directory
    
    * Delete web-development directory
    
    * Delete wordpress-development directory
    
    * Delete wp-content directory
    
    * Delete wp-includes directory
    
    * Delete wp-json directory
    
    * Delete .DS_Store
    
    * Delete CNAME
    
    * Delete README.md
    
    * Delete index.html
    
    * delete action file
    
    * Create node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * change yml file
    
    * Update node.js.yml
    
    * change config
    
    * deploy
    
    * deploy
    
    * Update next.config.js
    
    * change base path to /

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  basePath: "",
+  assetPrefix: "",
+}
+
+module.exports = nextConfig
