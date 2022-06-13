12e624f9877bcac116d0b1dd0254052a0d020c0b
abdullah4244 <62391274+abdullah4244@users.noreply.github.com>
2022-06-13 16:17:07 +0500

    Integrated Ts and prettier. (#41)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
+import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 import '../components/header/header.scss'
 import '../styles/pagesStyles/home.scss'
 import '../components/footer/footer.scss'
 import '../styles/pagesStyles/contactUs.scss'
 import '../styles/pagesStyles/careers.scss'
-import { createTheme, ThemeProvider, responsiveFontSizes } from '@mui/material/styles'
+import {
+  createTheme,
+  ThemeProvider,
+  responsiveFontSizes,
+} from '@mui/material/styles'
 import Head from 'next/head'
 import Layout from '../components/layout/Layout'
 import favicon from '../public/favicon.ico'
@@ -26,12 +31,12 @@ let theme = createTheme({
 
 theme = responsiveFontSizes(theme)
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="shortcut icon" type="image/x-icon" href={favicon.src}/>
-      </Head>
+        <link rel="shortcut icon" type="image/x-icon" href={favicon} />
+      </Head>{' '}
       <ThemeProvider theme={theme}>
         <Layout>
           <Component {...pageProps} />
