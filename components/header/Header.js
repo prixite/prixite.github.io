40ca89f79c74cfc048c61a29a6ffa2d44b5d743f
rkoalsi <57904969+rkoalsi@users.noreply.github.com>
2022-06-15 13:40:14 +0530

    Fixing issue #53 - added Head component to Header.js as well as removed it from other pages

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,20 +1,30 @@
-import { AppBar, Box, Container, IconButton, Menu, Toolbar, Typography, MenuItem, Button } from '@mui/material'
+import {
+  AppBar,
+  Box,
+  Container,
+  IconButton,
+  Menu,
+  Toolbar,
+  Typography,
+  MenuItem,
+  Button,
+} from '@mui/material'
 import Image from 'next/image'
 import React from 'react'
 import logoImg from '../../public/images/logo.png'
-import MenuIcon from "@mui/icons-material/Menu";
-import { pages } from '../../constants/constants';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-
-const Header = ({ children, bgImg }) => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
+import MenuIcon from '@mui/icons-material/Menu'
+import { pages } from '../../constants/constants'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import Head from 'next/head'
+const Header = ({ children, bgImg, title }) => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null)
 
   const router = useRouter()
 
   const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
+    setAnchorElNav(event.currentTarget)
+  }
 
   const handleCloseNavMenu = (page) => {
     if (page.text === 'Home') {
@@ -26,101 +36,141 @@ const Header = ({ children, bgImg }) => {
     }
 
     setAnchorElNav(null)
-  };
+  }
 
   return (
-    <Box
-      className='header-container'
-      sx={{
-        background: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${bgImg?.src})`,
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-      }}
-    >
-      <AppBar position='static' className='header-bar'>
-        <Container maxWidth='xl'>
-          <Toolbar disableGutters>
-            <Box sx={{ mr: 2, display: { xs: "none", md: "flex" }, cursor: 'pointer' }} >
-              <Image src={logoImg} alt='Prixite' onClick={() => router.push('/')} />
-            </Box>
-            <Box sx={{ flexGrow: 1 }}></Box>
-            <Box sx={{ flexGrow: 50, display: { xs: "flex", md: "none" }, cursor: 'pointer' }} >
-              <Image src={logoImg} alt="Prixite" onClick={() => router.push('/')} />
-            </Box>
-            <Box sx={{ flexGrow: 0, display: { xs: "flex", md: "none" } }} >
-              <IconButton
-                size='large'
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="prixite" content="Prixte" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Box
+        className="header-container"
+        sx={{
+          background: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${bgImg?.src})`,
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }}
+      >
+        <AppBar position="static" className="header-bar">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <Box
+                sx={{
+                  mr: 2,
+                  display: { xs: 'none', md: 'flex' },
+                  cursor: 'pointer',
                 }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{ display: { xs: "block", md: "none" } }}
               >
-                {pages.map((page, index) => (
-                  <MenuItem key={index} onClick={() => handleCloseNavMenu(page)}>
-                    <Typography textAlign="center">{page.text}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
+                <Image
+                  src={logoImg}
+                  alt="Prixite"
+                  onClick={() => router.push('/')}
+                />
+              </Box>
+              <Box sx={{ flexGrow: 1 }}></Box>
+              <Box
+                sx={{
+                  flexGrow: 50,
+                  display: { xs: 'flex', md: 'none' },
+                  cursor: 'pointer',
+                }}
+              >
+                <Image
+                  src={logoImg}
+                  alt="Prixite"
+                  onClick={() => router.push('/')}
+                />
+              </Box>
+              <Box sx={{ flexGrow: 0, display: { xs: 'flex', md: 'none' } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{ display: { xs: 'block', md: 'none' } }}
+                >
+                  {pages.map((page, index) => (
+                    <MenuItem
+                      key={index}
+                      onClick={() => handleCloseNavMenu(page)}
+                    >
+                      <Typography textAlign="center">{page.text}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
 
-            <Box
-              className='header-menu'
-              sx={{ display: { xs: 'none', md: 'flex' } }}
-            >
-              {pages.map((page, index) => {
-                if (page.text == 'Contact Us') {
+              <Box
+                className="header-menu"
+                sx={{ display: { xs: 'none', md: 'flex' } }}
+              >
+                {pages.map((page, index) => {
+                  if (page.text == 'Contact Us') {
+                    return (
+                      <Link
+                        href={page?.path}
+                        sx={{ textDecoration: 'none' }}
+                        key={index}
+                        passHref
+                      >
+                        <Button className="contact-us-button" sx={{ mx: 2 }}>
+                          {page.text}
+                        </Button>
+                      </Link>
+                    )
+                  }
                   return (
-                    <Link href={page?.path} sx={{ textDecoration: 'none' }} key={index} passHref>
-                      <Button
-                        className='contact-us-button'
-                        sx={{ mx: 2 }}
+                    <Link
+                      href={page?.path}
+                      sx={{ textDecoration: 'none' }}
+                      key={index}
+                      passHref
+                    >
+                      <Typography
+                        className="header-menu-items"
+                        sx={{
+                          mx: 2,
+                          color:
+                            router.asPath == page.path
+                              ? 'var(--primary-green)'
+                              : 'white',
+                        }}
+                        textAlign="center"
                       >
                         {page.text}
-                      </Button>
+                      </Typography>
                     </Link>
                   )
-                }
-                return (
-                  <Link href={page?.path} sx={{ textDecoration: 'none' }} key={index} passHref>
-                    <Typography
-                      className='header-menu-items'
-                      sx={{
-                        mx: 2,
-                        color: router.asPath == page.path ? "var(--primary-green)" : "white",
-                      }}
-                      textAlign='center'
-                    >
-                      {page.text}
-                    </Typography>
-                  </Link>
-                )
-              })}
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-      {children}
-    </Box>
+                })}
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+        {children}
+      </Box>
+    </>
   )
 }
 
--- pages/web-development.tsx
+++ pages/web-development.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Head from 'next/head'
 import Header from '../components/header/Header'
 import { Container, Stack, Box, Typography, Divider } from '@mui/material'
 import { webDevelopment } from '../constants/constants'
@@ -8,28 +7,23 @@ import webDevelopmentBgImg from '../public/images/webDevelopment.jpg'
 import Image from 'next/image'
 
 const Webdevelopment = () => {
-  const { title, header, heading, description } = webDevelopment;
+  const { title, header, heading, description } = webDevelopment
   return (
     <>
-      <Head>
-        <title>{title}</title>
-        <meta name="prixite" content="Prixte" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header bgImg={webDevelopmentBgImg}>
+      <Header bgImg={webDevelopmentBgImg} title={title}>
         <Container maxWidth="xl">
           <Stack
-            pt={{ xs: "80px", lg: "200px" }}
-            pb={{ xs: "100px", lg: "200px" }}
-            direction={{ xs: "column", md: "row" }}
+            pt={{ xs: '80px', lg: '200px' }}
+            pb={{ xs: '100px', lg: '200px' }}
+            direction={{ xs: 'column', md: 'row' }}
             spacing={5}
           >
             <Box>
               <Typography
                 variant="h3"
                 sx={{
-                  fontWeight: "bold",
-                  color: "white",
+                  fontWeight: 'bold',
+                  color: 'white',
                 }}
               >
                 {header}
@@ -37,18 +31,18 @@ const Webdevelopment = () => {
               <Divider
                 textAlign="left"
                 sx={{
-                  width: "17%",
-                  marginTop: "2rem",
-                  borderColor: "var(--primary-green)",
-                  borderWidth: "1px",
+                  width: '17%',
+                  marginTop: '2rem',
+                  borderColor: 'var(--primary-green)',
+                  borderWidth: '1px',
                 }}
               />
               <Typography
                 variant="h6"
                 sx={{
-                  fontWeight: "600",
-                  marginTop: "1rem",
-                  color: "white",
+                  fontWeight: '600',
+                  marginTop: '1rem',
+                  color: 'white',
                 }}
               >
                 {heading}
@@ -74,11 +68,13 @@ const Webdevelopment = () => {
           flexDirection: 'column',
           gap: 4,
           py: 10,
-          opacity: 0.8
+          opacity: 0.8,
         }}
-        maxWidth='xl'
+        maxWidth="xl"
       >
-        {description.map((item, index) => <Typography key={index}>{item}</Typography>)}
+        {description.map((item, index) => (
+          <Typography key={index}>{item}</Typography>
+        ))}
       </Container>
     </>
   )
