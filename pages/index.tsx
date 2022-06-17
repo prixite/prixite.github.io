991f0adc643c2aa31af9d57a5fee6230e2e44179
Umair Khan <umair.waheed@gmail.com>
2022-06-17 13:56:43 +0500

    Run prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,64 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/header/Header'
-import homePageBg from "../public/images/homePageBg.png";
-import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
-import { Box, Container, Typography, Stack, Divider, Grid, Button } from '@mui/material';
-import { homeData, services, servicesHeading } from '../constants/constants';
-import { useRouter } from 'next/router';
-
+import homePageBg from '../public/images/homePageBg.png'
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt'
+import {
+  Box,
+  Container,
+  Typography,
+  Stack,
+  Divider,
+  Grid,
+  Button,
+} from '@mui/material'
+import { homeData, services, servicesHeading } from '../constants/constants'
+import { useRouter } from 'next/router'
 
 export default function Home() {
-  const { title, header, headerImage, heading, descriptionHeading, paragraphs } = homeData;
+  const {
+    title,
+    header,
+    headerImage,
+    heading,
+    descriptionHeading,
+    paragraphs,
+  } = homeData
 
   const router = useRouter()
   return (
-    <div className='home-page-container'>
-
+    <div className="home-page-container">
       <Head>
         <title>{title}</title>
-        <meta name='prixite' content='Prixite' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="prixite" content="Prixite" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header bgImg={homePageBg}>
-        <Container maxWidth='xl'>
+        <Container maxWidth="xl">
           <Stack
-            pt={{ xs: "60px", lg: "110px" }}
-            pb={{ xs: "100px", lg: "80px" }}
-            direction={{ xs: "column", md: "row" }}
+            pt={{ xs: '60px', lg: '110px' }}
+            pb={{ xs: '100px', lg: '80px' }}
+            direction={{ xs: 'column', md: 'row' }}
             spacing={8}
           >
-
             <Box>
-              <Typography variant='h6' className='home-main-heading-1'>
+              <Typography variant="h6" className="home-main-heading-1">
                 {header}
               </Typography>
 
-              <Divider textAlign='left' className='divider' sx={{ width: '17%' }} />
+              <Divider
+                textAlign="left"
+                className="divider"
+                sx={{ width: '17%' }}
+              />
 
-              <Typography variant='h4' className='home-main-heading-2'>
+              <Typography variant="h4" className="home-main-heading-2">
                 {heading}
               </Typography>
             </Box>
 
-            <Box textAlign='center'>
+            <Box textAlign="center">
               <Image
                 unoptimized
                 priority
@@ -55,75 +71,55 @@ export default function Home() {
           </Stack>
         </Container>
       </Header>
-      <Container maxWidth='xl'>
-
+      <Container maxWidth="xl">
         <Box p="5rem 1.5rem">
-
           <Typography variant="h5" fontWeight="600">
             {descriptionHeading}
           </Typography>
 
           {paragraphs.map((item, index) => (
-            <Typography 
-              margin="1.8rem 0" 
-              key={index}>
-                {item.para}
+            <Typography margin="1.8rem 0" key={index}>
+              {item.para}
             </Typography>
-          )
-          )}
+          ))}
 
-          <Typography 
-            variant="h4" 
-            className='service-heading'>
-              {servicesHeading}
+          <Typography variant="h4" className="service-heading">
+            {servicesHeading}
           </Typography>
 
-          <Divider 
-            variant="middle" 
-            className='service-divider' 
-            sx={{ mx: 'auto', fill: 'true', width: '8%' }} 
+          <Divider
+            variant="middle"
+            className="service-divider"
+            sx={{ mx: 'auto', fill: 'true', width: '8%' }}
           />
 
-          <Grid 
-            container 
-            spacing={{ xs: 2, md: 3 }} 
-            columnSpacing={10}
-          >
+          <Grid container spacing={{ xs: 2, md: 3 }} columnSpacing={10}>
             {services.map((item, index) => (
-              <Grid 
-                item 
-                sm={12} 
-                md={6} 
-                key={index}
-              >
+              <Grid item sm={12} md={6} key={index}>
                 <Box marginBottom="10px">
-                  <div className='service-image'>
-                    <Image 
-                      src={item.img} 
-                      objectFit='cover' 
-                      width={800} 
-                      height={500} 
-                      className='service-image' 
-                      onClick={() => router.push(item.path)} 
+                  <div className="service-image">
+                    <Image
+                      src={item.img}
+                      objectFit="cover"
+                      width={800}
+                      height={500}
+                      className="service-image"
+                      onClick={() => router.push(item.path)}
                       alt="service-image"
                     />
                   </div>
 
-                  <Typography 
-                    variant="h6" 
-                    fontWeight="600" 
-                    mt='20px'>
-                      {item.title}
+                  <Typography variant="h6" fontWeight="600" mt="20px">
+                    {item.title}
                   </Typography>
 
-                  <Typography 
-                    className='service-description'>
-                      {item.desc}
+                  <Typography className="service-description">
+                    {item.desc}
                   </Typography>
 
                   <Box textAlign="center" mt="2rem">
                     <Button
-                      className='service-button'
+                      className="service-button"
                       endIcon={<ArrowRightAltIcon />}
                       onClick={() => router.push(item.path)}
                     >
--- pages/scraping.tsx
+++ pages/scraping.tsx
@@ -8,89 +8,89 @@ import { Container, Stack, Box, Typography, Divider } from '@mui/material'
 import Image from 'next/image'
 
 const scraping = () => {
-    const { title, header, heading, description, paraList } = scrapingData
+  const { title, header, heading, description, paraList } = scrapingData
 
-    return (
-        <>
-            <Head>
-                <title>{title}</title>
-                <meta name="prixite" content="Prixte" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Header bgImg={scrapingImage}>
-                <Container maxWidth="xl">
-                    <Stack
-                        pt={{ xs: "80px", lg: "200px" }}
-                        pb={{ xs: "100px", lg: "200px" }}
-                        direction={{ xs: "column", md: "row" }}
-                        spacing={5}
-                    >
-                        <Box>
-                            <Typography
-                                variant="h3"
-                                sx={{
-                                    fontWeight: "bold",
-                                    color: "white",
-                                }}
-                            >
-                                {header}
-                            </Typography>
-                            <Divider
-                                textAlign="left"
-                                sx={{
-                                    width: "17%",
-                                    marginTop: "2rem",
-                                    borderColor: "var(--primary-green)",
-                                    borderWidth: "1px",
-                                }}
-                            />
-                            <Typography
-                                variant="h6"
-                                sx={{
-                                    fontWeight: "600",
-                                    marginTop: "1rem",
-                                    color: "white",
-                                }}
-                            >
-                                {heading}
-                            </Typography>
-                        </Box>
-                        <Box textAlign="center">
-                            <Image
-                                unoptimized
-                                priority
-                                src={webScrapingImage}
-                                width={800}
-                                height={450}
-                                alt="service-image"
-                            />
-                        </Box>
-                    </Stack>
-                </Container>
-            </Header>
-
-            <Container
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="prixite" content="Prixte" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header bgImg={scrapingImage}>
+        <Container maxWidth="xl">
+          <Stack
+            pt={{ xs: '80px', lg: '200px' }}
+            pb={{ xs: '100px', lg: '200px' }}
+            direction={{ xs: 'column', md: 'row' }}
+            spacing={5}
+          >
+            <Box>
+              <Typography
+                variant="h3"
                 sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    gap: 4,
-                    py: 10,
-                    opacity: 0.8
+                  fontWeight: 'bold',
+                  color: 'white',
                 }}
-                maxWidth='xl'
-            >
-                <Typography>{description}</Typography>
+              >
+                {header}
+              </Typography>
+              <Divider
+                textAlign="left"
+                sx={{
+                  width: '17%',
+                  marginTop: '2rem',
+                  borderColor: 'var(--primary-green)',
+                  borderWidth: '1px',
+                }}
+              />
+              <Typography
+                variant="h6"
+                sx={{
+                  fontWeight: '600',
+                  marginTop: '1rem',
+                  color: 'white',
+                }}
+              >
+                {heading}
+              </Typography>
+            </Box>
+            <Box textAlign="center">
+              <Image
+                unoptimized
+                priority
+                src={webScrapingImage}
+                width={800}
+                height={450}
+                alt="service-image"
+              />
+            </Box>
+          </Stack>
+        </Container>
+      </Header>
 
-                {paraList.map((item, index) => (
-                    <Box key={index}>
-                        <ul className='scraping-list'>
-                            <li style={{ lineHeight: '25px' }}>{item}</li>
-                        </ul>
-                    </Box>
-                ))}
-            </Container>
-        </>
-    )
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 4,
+          py: 10,
+          opacity: 0.8,
+        }}
+        maxWidth="xl"
+      >
+        <Typography>{description}</Typography>
+
+        {paraList.map((item, index) => (
+          <Box key={index}>
+            <ul className="scraping-list">
+              <li style={{ lineHeight: '25px' }}>{item}</li>
+            </ul>
+          </Box>
+        ))}
+      </Container>
+    </>
+  )
 }
 
 export default scraping
