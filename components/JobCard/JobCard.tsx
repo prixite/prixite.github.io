348c69d53fc55ded07a3a759ad448dd36fdc5255
uzairmirza <53081780+uzairmirza90@users.noreply.github.com>
2022-06-22 15:16:26 +0500

    Jobs page added. (#45)
    * favicon error fix
    
    * npm install
    
    * change route configs
    
    * jobs page added
    
    * scss file added
    
    * scss file added
    
    * commit
    
    * pre-commit
    
    * Delete venv directory
    
    * added yarn.lock file

---
+++ components/JobCard/JobCard.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Box, Typography, Container, Stack } from '@mui/material'
+import Image from 'next/image'
+
+interface JobCardProps {
+  name: string
+  image: string
+  description: string
+}
+
+const JobCard: React.FC<JobCardProps> = ({ name, image, description }) => {
+  return (
+    <Container maxWidth="md" className="job-card">
+      <Stack className="job-card-items" direction={{ xs: 'column', md: 'row' }}>
+        <Box>
+          <Image
+            src={image}
+            width={150}
+            height={150}
+            objectFit="cover"
+            alt="Job"
+          />
+        </Box>
+        <Box className="job-card-content">
+          <Box>
+            <Typography variant="h6">{name}</Typography>
+          </Box>
+
+          <Box>
+            <Typography>{description}</Typography>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
+  )
+}
+
+export default JobCard
