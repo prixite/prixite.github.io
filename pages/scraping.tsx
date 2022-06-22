f2966bd40fe9e1f280561ea6c65a2cfa3bd460ef
Michał Struck <98230431+michalstruck@users.noreply.github.com>
2022-06-22 07:01:30 +0200

    fix #66 - Constants to data (#71)
    * fix: file typed
    
    * final
    
    * fix: changed constants to data, updated imports
    
    * chore: added missing imports, removed unused ones

--- pages/scraping.tsx
+++ pages/scraping.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { scrapingData } from '../constants/constants'
+import { scrapingData } from '../constants/data'
 import Header from '../components/header/Header'
 import scrapingImage from '../public/images/scraping.jpg'
 import webScrapingImage from '../public/images/wordpress Development-01.png'
