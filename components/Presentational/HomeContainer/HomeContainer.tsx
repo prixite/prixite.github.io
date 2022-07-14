57fc95cea89f2c16f8e3f451d5a05a087ed165b9
uzairmirza <53081780+uzairmirza90@users.noreply.github.com>
2022-07-14 18:23:22 +0500

    added news and blogs section

---
+++ components/Presentational/HomeContainer/HomeContainer.tsx
@@ -0,0 +1,55 @@
+import { Stack, Box, Typography } from '@mui/material'
+import React from 'react'
+import ContactButton from '../../Smart/ContactButton/ContactButton'
+import Image from 'next/image'
+import { homeData } from '../../../data/data'
+
+const HomeContainer = () => {
+  const {
+    header,
+    headerImage,
+    heading,
+    homeAim,
+    contactUsButtonText,
+    videoButtonText,
+    youtubeIcon,
+  } = homeData
+  return (
+    <Stack
+      pt={{ xs: '60px', lg: '60px' }}
+      pb={{ xs: '100px', lg: '60px' }}
+      direction={{ xs: 'column', md: 'row' }}
+      spacing={8}
+      alignItems={'center'}
+    >
+      <Box>
+        <Typography variant="h6" className="home-main-heading-1">
+          {header}
+        </Typography>
+
+        <Typography variant="h4" className="home-main-heading-2">
+          {heading.slice(0, 8)}
+          <span className="solution-color">{heading.slice(8, 17)}</span>
+          {heading.slice(17, 40)} 🚀
+        </Typography>
+
+        <Typography>{homeAim}</Typography>
+
+        <Box display="flex" gap="25px" alignItems={'center'} mt={6}>
+          <ContactButton text={contactUsButtonText} />
+
+          <Box className="youtube-icon">
+            <Image unoptimized priority src={youtubeIcon} alt="service-image" />
+          </Box>
+          <Typography>{videoButtonText}</Typography>
+        </Box>
+      </Box>
+
+      <Box>
+        <Image unoptimized priority src={headerImage} alt="service-image" />
+      </Box>
+    </Stack>
+  )
+}
+
+export default HomeContainer
--- data/data.ts
+++ data/data.ts
@@ -8,10 +8,10 @@ import pwaImage from '../public/images/pwaIcon.png'
 import reactDevImage from '../public/images/reactIcon.png'
 import homeHeaderImage from '../public/images/headerImage.png'
 import wall1 from '../public/images/wall1.jpg'
-import news1 from '../public/images/news1.png'
-import news2 from '../public/images/news2.png'
-import news3 from '../public/images/news3.png'
-import news4 from '../public/images/news4.png'
+import news1 from '../public/images/news1.svg'
+import news2 from '../public/images/news2.svg'
+import news3 from '../public/images/news3.svg'
+import news4 from '../public/images/news4.svg'
 import youtubeIcon from '../public/images/youtubeIcon.png'
 import InstagramIcon from '../public/images/instagram-icon.png'
 import FacebookIcon from '../public/images/facebook-icon.png'
@@ -78,43 +78,47 @@ export const homeData = {
   ],
 }
 
-
-
 /**********           News and Blogs Data             ********* */
 export const newsAndBlogs = {
   header: 'News & Blogs',
-  heading: 'We aim to constantly bring innovative solutions to cater to our client’s requirements and expectations.',
+  heading:
+    'We aim to constantly bring innovative solutions to cater to our client’s requirements and expectations.',
+  viewButtonText: 'View All',
+  mainBlog: {
+    image: news1,
+    title: 'In-house versus agency UX design',
+    description:
+      'Lorem ipsum is a placeholder text commonly used to demonstrate the visual. Text commonly used to demonstrate the visual',
+    chipLabel: 'UX Design',
+    readTime: '5 Mins Read',
+  },
   blogs: [
     {
       image: news2,
-      title: 'In-house versus agency UX design',
-      description: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual. Text commonly used to demonstrate the visual',
-      chipLabel: 'UX Design',
-      readTime: '5 Mins Read'
+      title: 'Fastest Growing IT Company',
+      description:
+        'Lorem ipsum is a placeholder text commonly used to demonstrate the visual. Text commonly used to demonstrate the visual',
+      chipLabel: 'Startup',
+      readTime: '5 Mins Read',
     },
     {
       image: news3,
-      title: 'In-house versus agency UX design',
-      description: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual. Text commonly used to demonstrate the visual',
-      chipLabel: 'UX Design',
-      readTime: '5 Mins Read'
+      title: 'Welcome to Prixite - Fastest Growing IT Company',
+      description:
+        'Lorem ipsum is a placeholder text commonly used to demonstrate the visual. Text commonly used to demonstrate the visual',
+      chipLabel: 'Mobile App Development',
+      readTime: '5 Mins Read',
     },
     {
       image: news4,
-      title: 'In-house versus agency UX design',
-      description: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual. Text commonly used to demonstrate the visual',
-      chipLabel: 'UX Design',
-      readTime: '5 Mins Read'
-    }
-  ]
+      title: 'Welcome to Prixite - Fastest Growing IT Company',
+      description:
+        'Lorem ipsum is a placeholder text commonly used to demonstrate the visual. Text commonly used to demonstrate the visual',
+      chipLabel: 'React Development',
+      readTime: '5 Mins Read',
+    },
+  ],
 }
-
-
-
-
-
-
-
 
 /**********            Home Page Services Data          ********* */
 export const servicesData = {
