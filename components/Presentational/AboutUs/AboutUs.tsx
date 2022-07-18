5322e7003ef18bb685375d807dd5c70ba8dc79ef
uzairmirza <53081780+uzairmirza90@users.noreply.github.com>
2022-07-18 17:54:13 +0500

    added about us content

--- components/Presentational/AboutUs/AboutUs.tsx
+++ components/Presentational/AboutUs/AboutUs.tsx
@@ -8,7 +8,19 @@ interface AboutUs {
 }
 
 const AboutUs: React.FC<AboutUs> = ({ header, heading, description }) => {
-  return <Box p="5rem 3rem" className="about-us"></Box>
+  return (
+    <Box p="5rem 3rem" className="about-us">
+      <Typography fontSize={30} fontWeight={600}>
+        {header.slice(0, 5)}
+        <span style={{ color: '#63AC45' }}>{header.slice(5, 8)}</span>
+      </Typography>
+
+      <Typography fontWeight={500}>{heading}</Typography>
+
+      <Divider className="about-us-divider" />
+      <Typography>{description}</Typography>
+    </Box>
+  )
 }
 
 export default AboutUs
