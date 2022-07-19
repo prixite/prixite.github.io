9b3fc9b24f66b98c16f2d98d72d4032574776aa5
uzairmirza <53081780+uzairmirza90@users.noreply.github.com>
2022-07-19 17:01:17 +0500

    added wordpress content

--- pages/wordpress-development.tsx
+++ pages/wordpress-development.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import { wordPressDevelopment } from '../data/data'
-import Header from '../components/Shared/header/Header'
-import WordPressBgImage from '../public/images/wall1.jpg'
-import WordPressImage from '../public/images/wordpress Development-01.png'
-import { Container, Stack, Divider, Box, Typography } from '@mui/material'
-import Image from 'next/image'
+import { Container, Box, Typography } from '@mui/material'
 import Head from 'next/head'
+import Subscribe from '../components/Shared/Subscribe/Subscribe'
+import ContactButton from '../components/Smart/ContactButton/ContactButton'
 
 const WordPressDevelopment = () => {
   const { title, header, heading, para1, para2, serviceHeading, services } =
@@ -17,78 +15,35 @@ const WordPressDevelopment = () => {
         <meta name="prixite" content="Prixte" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container maxWidth="xl">
-        <Stack
-          pt={{ xs: '80px', lg: '200px' }}
-          pb={{ xs: '100px', lg: '200px' }}
-          direction={{ xs: 'column', md: 'row' }}
-          spacing={5}
-        >
+      <Container maxWidth="xl" className="page-header">
+        <Box className="header">
+          <Box className="heading">
+            <Typography fontSize={30} fontWeight={600}>
+              {header.slice(0, 10)}
+              <span style={{ color: 'var(--primary-green)' }}>
+                {header.slice(10, 21)}
+              </span>
+            </Typography>
+            <Typography>{heading}</Typography>
+          </Box>
           <Box>
-            <Typography
-              variant="h3"
-              sx={{
-                fontWeight: 'bold',
-                color: 'white',
-              }}
-            >
-              {header}
-            </Typography>
-            <Divider
-              textAlign="left"
-              sx={{
-                width: '17%',
-                marginTop: '2rem',
-                borderColor: 'var(--primary-green)',
-                borderWidth: '1px',
-              }}
-            />
-            <Typography
-              variant="h6"
-              sx={{
-                fontWeight: '600',
-                marginTop: '1rem',
-                color: 'white',
-              }}
-            >
-              {heading}
-            </Typography>
+            <ContactButton text="Contact Us" />
           </Box>
-          <Box textAlign="center">
-            <Image
-              unoptimized
-              priority
-              src={WordPressImage}
-              width={800}
-              height={450}
-              alt="service-image"
-            />
-          </Box>
-        </Stack>
-      </Container>
+        </Box>
 
-      <Container
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 4,
-          py: 10,
-          opacity: 0.8,
-        }}
-        maxWidth="xl"
-      >
-        <Typography>{para1}</Typography>
-
-        <Typography>{serviceHeading}</Typography>
-
-        {services.map((item, index) => (
-          <Box key={index}>
-            <ul className="wordpress-list">
-              <li>{item}</li>
-            </ul>
-          </Box>
-        ))}
-        <Typography>{para2}</Typography>
+        <Container className="page-content" maxWidth="lg">
+          <Typography fontSize={18}>{para1}</Typography>
+          <Typography fontSize={18}>{serviceHeading}</Typography>
+          {services.map((service, index) => {
+            return (
+              <Typography key={index} lineHeight={0} fontSize={18}>
+                <li>{service}</li>
+              </Typography>
+            )
+          })}
+          <Typography fontSize={18}>{para2}</Typography>
+        </Container>
+        <Subscribe />
       </Container>
     </>
   )
