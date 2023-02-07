93a8e59b195aa1a3de49effc1cb46c2c8d1c9bdb
Uzair Jamil <53081780+uzairmirza90@users.noreply.github.com>
2023-02-07 11:51:05 +0500

    change feature flags path (#284)

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,9 +1,3 @@
 export const BLOGS_PATH = 'data/blogs'
 export const SERVICES_PATH = 'data/services'
 export const TESTIMONIALS_PATH = 'data/testimonials'
-
-export const FEATURES = {
-  blogs: true,
-  testimonials: true,
-  subscribe: false,
-}
