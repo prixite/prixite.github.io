22e188b4fd4b465c3ba1551cd05fc3cd456957f1
imrancs058 <imrancs058@yahoo.com>
2023-04-14 17:19:11 +0500

    completed Issue

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,6 +1,9 @@
 import { useEffect } from 'react'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 import fs from 'fs'
 import Image from 'next/image'
+import { LinkedinShareButton, FacebookShareButton } from 'next-share'
 import { Box, Container, Stack, Typography } from '@mui/material'
 import { joinUsLinkIcons, newsAndBlogs } from '../../data/data'
 import { BlogPostWithContent, Blog } from '../../types/interfaces'
@@ -23,79 +26,97 @@ import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace'
 import 'prismjs/components/prism-markup-templating'
 
 export default function BlogDetailPage({
-  frontmatter: { title, date },
+  frontmatter: { title, date, cover_image },
   content,
 }: BlogPostWithContent) {
+  const router = useRouter()
   useEffect(() => {
     Prism.highlightAll()
   }, [])
 
   return (
-    <Container maxWidth="xl" className="page-header">
-      <Box className="header">
-        <Box className="heading">
-          <Typography className="heading-text" sx={{ width: '95%' }}>
-            {title}
-          </Typography>
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:image" content={cover_image} />
+      </Head>
+      <Container maxWidth="xl" className="page-header">
+        <Box className="header">
+          <Box className="heading">
+            <Typography className="heading-text" sx={{ width: '95%' }}>
+              {title}
+            </Typography>
+          </Box>
+          <Stack direction={'row'} gap={3} alignItems="center">
+            <Typography fontSize={12}>{date}</Typography>
+            <Typography fontSize={12}>5 min read</Typography>
+            <Stack
+              direction={{ xs: 'column', md: 'column', lg: 'row' }}
+              alignItems="center"
+              gap={3}
+              className="icons"
+            >
+              <FacebookShareButton
+                url={`${process.env.NEXT_PUBLIC_BASEPATH}/${router.asPath}`}
+              >
+                <Image
+                  src={joinUsLinkIcons[0].icon.src}
+                  alt="icon"
+                  width={joinUsLinkIcons[0].width}
+                  height={joinUsLinkIcons[0].height}
+                  objectFit="cover"
+                />
+              </FacebookShareButton>
+              <LinkedinShareButton
+                url={
+                  'https://e963-39-40-250-233.ngrok-free.app/blog/annual-lunch-2023'
+                }
+              >
+                <Image
+                  src={joinUsLinkIcons[1].icon.src}
+                  alt="icon"
+                  width={joinUsLinkIcons[1].width}
+                  height={joinUsLinkIcons[1].height}
+                  objectFit="cover"
+                />
+              </LinkedinShareButton>
+            </Stack>
+          </Stack>
         </Box>
-        <Stack direction={'row'} gap={3} alignItems="center">
-          <Typography fontSize={12}>{date}</Typography>
-          <Typography fontSize={12}>5 min read</Typography>
+
+        <Container disableGutters maxWidth="xl">
+          <div className="post-body">
+            <MarkdownText>{content}</MarkdownText>
+          </div>
+        </Container>
+
+        <Container className="likes" maxWidth="xl">
+          <Stack direction={'row'} gap={10}>
+            <Stack direction={'row'} gap={1} alignItems="center">
+              <Typography fontSize={13}>Written by Waleed Raza</Typography>
+            </Stack>
+          </Stack>
           <Stack
-            direction={{ xs: 'column', md: 'column', lg: 'row' }}
+            direction={'row'}
+            gap={1}
             alignItems="center"
-            gap={3}
-            className="icons"
+            className="scroll-top"
+            style={{ cursor: 'pointer' }}
+            onClick={() => {
+              window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+            }}
           >
-            {joinUsLinkIcons.map(({ icon, path, width, height }, index) => (
-              <Box key={index}>
-                <a href={path} target="_blank" rel="noopener noreferrer">
-                  <Image
-                    src={icon.src}
-                    alt="icon"
-                    width={width}
-                    height={height}
-                    objectFit="cover"
-                  />
-                </a>
-              </Box>
-            ))}
+            <Image
+              src={newsAndBlogs.scrollToTopImg.src}
+              width={25}
+              height={25}
+              alt="icon"
+            />
+            <Typography fontSize={13}>Back To Top </Typography>
           </Stack>
-        </Stack>
-      </Box>
-
-      <Container disableGutters maxWidth="xl">
-        <div className="post-body">
-          <MarkdownText>{content}</MarkdownText>
-        </div>
+        </Container>
       </Container>
-
-      <Container className="likes" maxWidth="xl">
-        <Stack direction={'row'} gap={10}>
-          <Stack direction={'row'} gap={1} alignItems="center">
-            <Typography fontSize={13}>Written by Waleed Raza</Typography>
-          </Stack>
-        </Stack>
-        <Stack
-          direction={'row'}
-          gap={1}
-          alignItems="center"
-          className="scroll-top"
-          style={{ cursor: 'pointer' }}
-          onClick={() => {
-            window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
-          }}
-        >
-          <Image
-            src={newsAndBlogs.scrollToTopImg.src}
-            width={25}
-            height={25}
-            alt="icon"
-          />
-          <Typography fontSize={13}>Back To Top </Typography>
-        </Stack>
-      </Container>
-    </Container>
+    </>
   )
 }
 
