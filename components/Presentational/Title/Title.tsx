3c596fe47a6543adf9207ab07e2cc3f10252724a
Imran Afzal <imrancs058@yahoo.com>
2023-04-17 17:21:57 +0500

    SEO 2: Fix footer (#342)
    * Fixed About Us and  Contact Us Links, Added Services and Blogs Link, Make Prixite Logo Link and Updated date
    
    * Fix Footer Completed
    
    * Fix Footer Completed
    
    * Changed footer elements into links
    
    * Finalizing
    
    * Removed Pre-commit Error

--- components/Presentational/Title/Title.tsx
+++ components/Presentational/Title/Title.tsx
@@ -1,26 +1,22 @@
 import { Box, Typography } from '@mui/material'
-import router from 'next/router'
 import React from 'react'
 import { siteName } from '../../../data/data'
 import Image from 'next/image'
 import logoImg from '../../../public/images/prixite-logo.png'
+import Link from 'next/link'
 
 const Title = () => {
   return (
     <Box className="title-container">
-      <Box className="title-image">
-        <Image
-          src={logoImg.src}
-          alt={siteName}
-          onClick={() => router.push('/')}
-          width={30}
-          height={30}
-        />
-      </Box>
-      <Typography className="title-heading" onClick={() => router.push('/')}>
-        {siteName}
-        <span style={{ color: '#63AC45' }}>.</span>
-      </Typography>
+      <Link href="/" className="title-link">
+        <Box className="title-image">
+          <Image src={logoImg.src} alt={siteName} width={30} height={30} />
+        </Box>
+        <Typography className="title-heading">
+          {siteName}
+          <span style={{ color: '#63AC45' }}>.</span>
+        </Typography>
+      </Link>
     </Box>
   )
 }
--- components/Shared/footer/Footer.tsx
+++ components/Shared/footer/Footer.tsx
@@ -3,10 +3,9 @@ import { Box, Container, Divider, Stack, Typography } from '@mui/material'
 import { footerData } from '../../../data/data'
 import Title from '../../Presentational/Title/Title'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const Footer = () => {
-  const router = useRouter()
   const { copyright, info, joinUs, details } = footerData
 
   return (
@@ -19,7 +18,13 @@ const Footer = () => {
               <Box>
                 <Image src={icon.src} alt="icon" width={20} height={20} />
               </Box>
-              <Typography className="description">{desc}</Typography>
+              <Typography className="description">
+                {new RegExp(/^\S+@\S+\.\S+$/).test(desc) ? (
+                  <Link href={`mailto:${desc}`}>{desc}</Link>
+                ) : (
+                  desc
+                )}
+              </Typography>
             </Stack>
           ))}
         </Box>
@@ -28,13 +33,9 @@ const Footer = () => {
             {info.title}
           </Typography>
           {info.contactUs.map((contact, index) => (
-            <Typography
-              key={index}
-              onClick={() => router.push(contact.path)}
-              sx={{ cursor: 'pointer' }}
-            >
+            <Link key={index} href={contact.path}>
               {contact.text}
-            </Typography>
+            </Link>
           ))}
         </Box>
         <Box className="footer-items-container">
@@ -66,7 +67,10 @@ const Footer = () => {
       <Container maxWidth="xl">
         <Divider sx={{ marginTop: 4 }} />
         <Box className="copyright">
-          <Typography>{copyright}</Typography>
+          <Typography>
+            <Link href="/">{copyright.split(' ')[0]}&nbsp;</Link>
+            {copyright.split(' ').splice(1).toString().replaceAll(',', ' ')}
+          </Typography>
         </Box>
       </Container>
     </Box>
