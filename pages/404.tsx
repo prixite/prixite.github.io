3a8bf8f62c90ae4165f2f057f7a6322e5a15d712
Aleesha-azhar <aleesha.azhar01@gmail.com>
2023-07-04 11:17:20 +0500

    page not found

---
+++ pages/404.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <>
+      <div className="notfound-404">
+        <h2
+          style={{
+            width: '100%',
+            height: '45.5vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          404 | This Page could not be found
+        </h2>
+      </div>
+    </>
+  )
+}
+
+export default PageNotFound
