a06824734eb94cb10d0b70055b72c4dc95171330
umairwaheed <umair.waheed@gmail.com>
2023-07-04 16:43:09 +0000

    Deploying to deploy from @ prixite/prixite.github.io@486531f28ff61a711adf2d62cec916a74676f90d 🚀

---
+++ _next/static/uVs-MlI1HxacKVNKjH-BB/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout-us","\u002Fblog\u002F[slug]","\u002Fblogs","\u002Fproduct","\u002Fproduct\u002F[slug]","\u002Fservices","\u002Fservices\u002F[slug]","\u002Fteam","\u002Fteam\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
