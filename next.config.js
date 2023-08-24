dc88360ebdffa582c51ebd8361d0a6051afbd803
Aleesha-azhar <aleesha.azhar01@gmail.com>
2023-08-24 15:11:26 +0500

    fixed

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
   reactStrictMode: true,
   images: {
     loader: 'akamai',
